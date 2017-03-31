35154a7ac62b79688e96c598d5524d5ccfea4ccf
Mikajy401 <mikajy401@gmail.com>
2017-03-31 16:56:20 +0300

    Script d'initialisation de la bdd

---
+++ LemursPortal-web/src/main/resources/db-init.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO typeuser(id, libelle, description) VALUES 
+(10, 'USER', 'Simple Utilisateur'),
+(101, 'EXPERT', ''),
+(1001, 'MODERATEUR', 'Moderateur du site'),
+(10001, 'ADMIN', 'Administrateur du site');
