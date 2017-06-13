f1d0cd18e98563ef7b9acf1acdee900c9c263c6c
rzobienvenue <rzobienvenue@gmail.com>
2017-06-13 15:30:45 +0300

    initialize document_type

--- LemursPortal-web/src/main/resources/db-init.sql
+++ LemursPortal-web/src/main/resources/db-init.sql
@@ -3,3 +3,8 @@ insert into typeuser(id, libelle, description) VALUES
 (101, 'EXPERT', ''),
 (1001, 'MODERATEUR', 'Moderateur du site'),
 (10001, 'ADMIN', 'Administrateur du site');
+
+INSERT INTO type_document (id, libelle) VALUES (1, 'PHOTO');
+INSERT INTO type_document (id, libelle) VALUES (2, 'VIDEO');
+INSERT INTO type_document (id, libelle) VALUES (3, 'AUDIO');
+INSERT INTO type_document (id, libelle) VALUES (4, 'PUBLICATION');
